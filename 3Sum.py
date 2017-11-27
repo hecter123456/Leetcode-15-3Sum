95124c7bd4b45213db016c9f85568aee86896412
hecter_wu <hecter_wu@transcend.com.tw>
2017-11-27 10:22:12 +0800

    pass testcase: testSampleInput by brute force

--- 3Sum.py
+++ 3Sum.py
@@ -5,12 +5,28 @@ class unitest(unittest.TestCase):
         Input = [0,0]
         ans = []
         self.assertEqual(Solution().threeSum(Input),ans);
+    def testSampleInput(self):
+        Input = [-1, 0, 1, 2, -1, -4]
+        ans = [[-1, 0, 1],[-1, -1, 2]]
+        self.assertEqual(Solution().threeSum(Input),ans);
 
 class Solution():
     def threeSum(self, nums):
         if len(nums) < 3:
             return []
-        return None
+        nums.sort()
+        ans = []
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                for z in range(j+1,len(nums)):
+                    if nums[i] + nums[j] + nums[z] == 0:
+                        temp = [nums[i],nums[j],nums[z]]
+                        ans.append(temp)
+        newans = []
+        for i in ans:
+            if i not in newans:
+                newans.append(i)
+        return newans
 
 if __name__ == '__main__':
     unittest.main()
